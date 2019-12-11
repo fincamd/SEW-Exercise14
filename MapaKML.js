298ccdd6945f1e4b35259bc747bcf40c9a8743e5
Daniel Finca <37817126+fincamd@users.noreply.github.com>
2019-12-11 18:13:56 +0100

    Updates

--- MapaKML.js
+++ MapaKML.js
@@ -1,11 +1,27 @@
+var miMapa = new Object();
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('mapDiv'), {
-        zoom: 11,
-        center: { lat: 41.876, lng: -87.624 }
+    var centro = { lat: 53.136267, lng: 12.826394 };
+    var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapaDinamicoGoogleDeterminado'), {
+        zoom: 8,
+        center: centro,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     });
+
+    var marker = new google.maps.Marker({ position: centro, map: mapaGeoposicionado });
 
     var ctaLayer = new google.maps.KmlLayer({
         url: 'https://fincamd.github.io/SEW-KMLTask/rutas_turisticas_kml.xml',
         map: map
     });
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: Ha fallado la geolocalización' :
+        'Error: Su navegador no soporta geolocalización');
+    infoWindow.open(mapaGeoposicionado);
+}
+
+miMapa.initMap = initMap;
